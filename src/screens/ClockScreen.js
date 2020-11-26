23a661384e759e7ff30d1bf5b3e73eb9ed3ab3b2
cliid <jiwujang@naver.com>
2020-11-26 23:56:27 +0900

    Changed Clock to World Clock, Time to Clock

--- src/screens/ClockScreen.js
+++ src/screens/ClockScreen.js
@@ -1,31 +1,37 @@
-import React, {useEffect, useState} from 'react';
-import {View, StyleSheet} from 'react-native';
-import {Text} from 'react-native-paper';
+import React from 'react';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {useTheme} from 'react-native-paper';
 
-function ClockScreen() {
-  const [curTime, setCurTime] = useState();
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    time: {
-      fontSize: 50,
-      textAlign: 'center',
-      margin: 10,
-    },
-  });
+import WorldClockScreen from './WorldClockScreen';
+import TimerScreen from './TimerScreen';
+import StopwatchScreen from './StopwatchScreen';
 
-  useEffect(() => {
-    setInterval(() => setCurTime(new Date().toTimeString().split(' ')[0]), 10);
-  }, []);
+const Tab = createMaterialBottomTabNavigator();
 
+function TimeScreen() {
+  const {colors} = useTheme();
   return (
-    <View style={styles.container}>
-      <Text style={styles.time}>{curTime}</Text>
-    </View>
+    <Tab.Navigator
+      initialRouteName="World Clock"
+      backBehavior="none"
+      labeled={true}
+      barStyle={{backgroundColor: colors.background}}>
+      <Tab.Screen
+        name="World Clock"
+        component={WorldClockScreen}
+        options={{tabBarIcon: 'web-clock'}}
+      />
+      <Tab.Screen
+        name="Timer"
+        component={TimerScreen}
+        options={{tabBarIcon: 'av-timer'}}
+      />
+      <Tab.Screen
+        name="Stopwatch"
+        component={StopwatchScreen}
+        options={{tabBarIcon: 'timer-outline'}}
+      />
+    </Tab.Navigator>
   );
 }
-
-export default ClockScreen;
+export default TimeScreen;
---
+++ src/screens/WorldClockScreen.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {Text} from 'react-native-paper';
+
+function ClockScreen() {
+  const [curTime, setCurTime] = useState();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    time: {
+      fontSize: 50,
+      textAlign: 'center',
+      margin: 10,
+    },
+  });
+
+  useEffect(() => {
+    setInterval(() => setCurTime(new Date().toTimeString().split(' ')[0]), 10);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.time}>{curTime}</Text>
+    </View>
+  );
+}
+
+export default ClockScreen;
