3137d2dc83e474469334f7eeb62ca10f978dcec9
cliid <jiwujang@naver.com>
2020-11-27 23:47:56 +0900

    Added About Screen

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -4,32 +4,50 @@ import {useTheme} from '@react-navigation/native';
 
 function AboutScreen() {
   const {colors} = useTheme();
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: colors.background,
-    },
-    about: {
-      fontSize: 50,
-      textAlign: 'center',
-      color: colors.text,
-      margin: 10,
-    },
-  });
-
-  const state = {
-    timerOn: false,
-    timerStart: 0,
-    timerTime: 0,
-  };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.about}>TimerScreen</Text>
+    <View style={[styles.container, {backgroundColor: colors.background}]}>
+      <View style={[styles.centerText, {backgroundColor: colors.background}]}>
+        <Text style={[styles.about, {color: colors.text}]}>
+          Made with ❤ by:
+        </Text>
+        <Text style={[styles.about, {color: colors.text}]}>cliid & Shio.</Text>
+      </View>
+      <View
+        style={[
+          styles.jitcijkProductText,
+          {backgroundColor: colors.background},
+        ]}>
+        <Text style={[styles.about, {color: colors.text}]}>
+          A product of Jitcijk Inc.
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centerText: {
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  jitcijkProductText: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  about: {
+    fontSize: 20,
+    fontFamily: 'CircularStd-Book',
+    textAlign: 'center',
+    margin: 10,
+  },
+});
 
 export default AboutScreen;
