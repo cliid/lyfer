c0be59f72800ebd3875ab55a8922bdffc523036e
cliid <jiwujang@naver.com>
2020-11-28 14:37:57 +0900

    Revert "changed"
    This reverts commit 4227fa0b870c1d5b0ca4f089b8be2f572591e838.

--- src/screens/CalculatorScreen.js
+++ src/screens/CalculatorScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Dimensions, PanResponder, View, Text } from 'react-native';
+import {StyleSheet, Text, View, StatusBar, SafeAreaView} from 'react-native';
 import {useTheme} from '@react-navigation/native';
 
 function CalculatorScreen() {
@@ -20,8 +20,92 @@ function CalculatorScreen() {
     },
   });
 
-  
+  state = initialState;
 
+  handleTap = (type, value) => {
+    this.setState((state) => calculator(type, value, state));
+  };
+
+  render();
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView>
+        <Text style={styles.value}>
+          {parseFloat(this.state.currentValue).toLocaleString()}
+        </Text>
+        <Row>
+          <Button
+            text="C"
+            theme="secondary"
+            onPress={() => this.handleTap('clear')}
+          />
+          <Button
+            text="+/-"
+            theme="secondary"
+            onPress={() => this.handleTap('posneg')}
+          />
+          <Button
+            text="%"
+            theme="secondary"
+            onPress={() => this.handleTap('percentage')}
+          />
+          <Button
+            text="/"
+            theme="accent"
+            onPress={() => this.handleTap('operator', '/')}
+          />
+        </Row>
+
+        <Row>
+          <Button text="7" onPress={() => this.handleTap('number', 7)} />
+          <Button text="8" onPress={() => this.handleTap('number', 8)} />
+          <Button text="9" onPress={() => this.handleTap('number', 9)} />
+          <Button
+            text="x"
+            theme="accent"
+            onPress={() => this.handleTap('operator', '*')}
+          />
+        </Row>
+
+        <Row>
+          <Button text="4" onPress={() => this.handleTap('number', 4)} />
+          <Button text="5" onPress={() => this.handleTap('number', 5)} />
+          <Button text="6" onPress={() => this.handleTap('number', 6)} />
+          <Button
+            text="-"
+            theme="accent"
+            onPress={() => this.handleTap('operator', '-')}
+          />
+        </Row>
+
+        <Row>
+          <Button text="1" onPress={() => this.handleTap('number', 1)} />
+          <Button text="2" onPress={() => this.handleTap('number', 2)} />
+          <Button text="3" onPress={() => this.handleTap('number', 3)} />
+          <Button
+            text="+"
+            theme="accent"
+            onPress={() => this.handleTap('operator', '+')}
+          />
+        </Row>
+
+        <Row>
+          <Button
+            text="0"
+            size="double"
+            onPress={() => this.handleTap('number', 0)}
+          />
+          <Button text="." onPress={() => this.handleTap('number', '.')} />
+          <Button
+            text="="
+            theme="accent"
+            onPress={() => this.handleTap('equal')}
+          />
+        </Row>
+      </SafeAreaView>
+    </View>
+  );
 }
 
 export default CalculatorScreen;
